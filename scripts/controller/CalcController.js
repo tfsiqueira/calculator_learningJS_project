f12357b97067ba93c8b3994b08bbcf990bb6f7a4
tfsiqueira <thiagofelipesiqueira@gmail.com>
2020-02-09 20:04:10 -0300

    inserting equal button function

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
     //The constructor method is a special method for creating and initializing an object created with a class.
     constructor() {
 
+        this._lastOperator = '';
+        this._lastNumber = '';
         this._operation = [];
         this._locate = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -63,11 +65,6 @@ class CalcController {
 
     }
 
-    calc() {
-        let last = this._operation.pop();
-
-    }
-
     pushOperation(value) {
 
         this._operation.push(value);
@@ -78,21 +75,39 @@ class CalcController {
         }
     }
 
+    getResult() {
+
+        return eval(this._operation.join(""));
+    }
 
     calc() {
 
         let last = '';
 
-        if (this._operation.length > 3) {
-            let last = this._operation.pop();
+        this._lastOperator = this.getLastItem();
+
+        if (this._operation.length < 3) {
+
+            let firstItem = this._operation[0];
+            this._operation = [firstItem, this._lastOperator, this._lastNumber];
 
         }
 
-        let result = eval(this._operation.join(""));
+        if (this._operation.length > 3) {
+
+            let last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        } else if (this._operation.length == 3) {
+
+            this._lastNumber = this.getLastItem(false);
+        }
+
+        let result = this.getResult();
 
         if (last == '%') {
 
-            result /= 10;
+            result /= 100;
             this._operation = [result];
 
         } else {
@@ -107,17 +122,29 @@ class CalcController {
         this.setLastNumberToDisplay();
     }
 
-    setLastNumberToDisplay() {
+    getLastItem(isOperator = true) {
 
-        let lastNumber;
+        let lastItem;
         for (let i = this._operation.length - 1; i >= 0; i--) {
 
-            if (!this.isOperator(this._operation[i])) {
-
-                lastNumber = this._operation[i];
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i];
                 break;
             }
         }
+
+        if (!lastItem) {
+
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+
+        return lastItem;
+
+    }
+
+    setLastNumberToDisplay() {
+
+        let lastNumber = this.getLastItem(false);
 
         if (!lastNumber) lastNumber = 0;
 
@@ -169,8 +196,6 @@ class CalcController {
 
         this.displayCalc = "ERROR";
     }
-
-
 
     execBtn(value) {
 
@@ -237,7 +262,6 @@ class CalcController {
 
         }
     }
-
 
     initButtonsEvents() {
 
